f5b8d29622c09cbadbdea6c723760d03895c7c4e
Iván López <lopez.ivan@gmail.com>
2016-10-14 10:15:43 +0200

    Java-DSL: Example 1

--- java-dsl/src/main/java/com/example/DemoApplication.java
+++ java-dsl/src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.endpoint.MethodInvokingMessageSource;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 @IntegrationComponentScan
@@ -11,4 +21,39 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Bean
+    public MessageSource<?> integerMessageSource() {
+        MethodInvokingMessageSource source = new MethodInvokingMessageSource();
+        source.setObject(new AtomicInteger());
+        source.setMethodName("getAndIncrement");
+        return source;
+    }
+
+    @Bean
+    DirectChannel inputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public IntegrationFlow myFlow() {
+        return IntegrationFlows
+            .from(this.integerMessageSource(), c -> c.poller(Pollers.fixedDelay(1000)))
+            .channel(this.inputChannel())
+            .filter((Integer i) -> i > 0)
+            .transform(Object::toString)
+            .channel(this.outputChannel())
+            .get();
+    }
+
+    @Bean
+    DirectChannel outputChannel() {
+        return new DirectChannel();
+    }
+
+    @ServiceActivator(inputChannel = "outputChannel")
+    public void consumir(String s) {
+        System.out.println("El número es: " + s);
+    }
+
 }
